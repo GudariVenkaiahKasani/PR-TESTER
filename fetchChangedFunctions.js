6817d70f3126cb1dbaa50c8d9fcf67cda3535224
Kasani Gudari Venkaiah <gudari183@gmail.com>
2025-09-30 14:16:37 +0530

    Update fetchChangedFunctions.js

--- fetchChangedFunctions.js
+++ fetchChangedFunctions.js
@@ -29,7 +29,8 @@ function parseDiff(patch) {
  */
 async function fetchGithubPRChangedFunctions(owner, repo, pull_number, token) {
   const changedFunctionsMap = new Map();
-
+  const newVariable=124:
+  const secondVariable=167:
   console.log(`🔄 Fetching PR #${pull_number} for ${owner}/${repo}...`);
 
   // 1️⃣ Get PR file diffs
@@ -102,3 +103,4 @@ async function fetchGithubPRChangedFunctions(owner, repo, pull_number, token) {
 }
 
 module.exports = { fetchGithubPRChangedFunctions };
+
