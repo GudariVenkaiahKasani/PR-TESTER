9fde0358500a77115b60e7a0352389a288b6c3e8
Kasani Gudari Venkaiah <gudari183@gmail.com>
2025-10-01 12:27:45 +0530

    Update fetchChangedFunctions.js

--- fetchChangedFunctions.js
+++ fetchChangedFunctions.js
@@ -8,7 +8,7 @@ const traverse = require("@babel/traverse").default;
 function parseDiff(patch) {
   const lines = patch.split("\n");
   const changed = [];
-  let currentLine = 0;
+const newlineAdded=0
 
   for (const line of lines) {
     if (line.startsWith("@@")) {
@@ -102,3 +102,4 @@ async function fetchGithubPRChangedFunctions(owner, repo, pull_number, token) {
 }
 
 module.exports = { fetchGithubPRChangedFunctions };
+
