07c35a566d8369cd78da51ff46a6a3c8b7de9f2e
Kasani Gudari Venkaiah <gudari183@gmail.com>
2025-10-02 20:50:40 +0530

    Update fetchChangedFunctions.js

--- fetchChangedFunctions.js
+++ fetchChangedFunctions.js
@@ -9,6 +9,7 @@ export function parseDiff(patch) {
   const lines = patch.split("\n");
   const changed = [];
   const add=0
+  const newAdd=0
   let currentLine = 0;
 
   for (const line of lines) {
@@ -103,5 +104,6 @@ async function fetchGithubPRChangedFunctions(owner, repo, pull_number, token) {
 }
 
 module.exports = { fetchGithubPRChangedFunctions };
+
 
 
